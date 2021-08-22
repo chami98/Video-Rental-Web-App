7a598d5f99dfff9fbf1f929ec343d27b60b3eef5
chamikara <chamikara98@gmail.com>
2021-08-22 23:40:31 +0530

    Extracted a reusable Input

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,24 +1,47 @@
 import React, { Component } from "react";
+import reactDom from "react-dom";
+import Input from "./common/input";
 
 class LoginForm extends Component {
+  state = {
+    account: {
+      username: "",
+      password: "",
+    },
+  };
+
+  username = React.createRef();
+
   handleSubmit = (e) => {
     e.preventDefault();
     console.log("Submitted!");
   };
 
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
+
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
-          </div>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+          />
+         <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+          />
           <button className="btn btn-primary">Submit</button>
         </form>
       </div>
